21f5cfe94f8b19fe662fc715b73fd8dd58d9773a
Jacob118 <871230295@qq.com>
2019-06-24 11:58:33 +0800

    DpsQuery4.0

--- app/src/main/java/cn/edu/swufe/dpsquery/SummonerActivity.java
+++ app/src/main/java/cn/edu/swufe/dpsquery/SummonerActivity.java
@@ -19,56 +19,78 @@ import java.util.Map;
 
 public class SummonerActivity extends ListActivity implements AdapterView.OnItemClickListener {
 
-    private ListView listView;
-    private String[] name={"欧米茄零式时空狭缝 阿尔法篇一层","欧米茄零式时空狭缝 阿尔法篇二层","欧米茄零式时空狭缝 阿尔法篇三层","欧米茄零式时空狭缝 阿尔法篇四层"};
-    private int[] images={R.drawable.ninesss,R.drawable.tensss,R.drawable.elevensss,R.drawable.twelvesss};
-
-    private List<Map<String,Object>> list_map=new ArrayList<Map<String,Object>>();
+    private ArrayList<HashMap<String,Object>> listItems;
+    private SimpleAdapter listItemAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blackmage);
+        initListView();
+        this.setListAdapter(listItemAdapter);
+        //setContentView(R.layout.activity_blackmage);
 
-        listView = (ListView) findViewById(R.id.listView);
+        getListView().setOnItemClickListener(this);
 
-        for (int i = 0; i < 5; i++) {
-            HashMap<String, Object> items = new HashMap<String, Object>();
 
-            items.put("pic", images[i]);
-            items.put("name", name[i]);
-            list_map.add(items);
         }
+        private void initListView(){
+        listItems =new ArrayList<HashMap<String, Object>>();
+        HashMap<String,Object> map1=new HashMap<String, Object>();
+        map1.put("pic",R.drawable.ninesss);
+        map1.put("text","欧米茄零式时空狭缝 阿尔法篇一层");
+
+            HashMap<String,Object> map2=new HashMap<String, Object>();
+            map2.put("pic",R.drawable.tensss);
+            map2.put("text","欧米茄零式时空狭缝 阿尔法篇二层");
+
+            HashMap<String,Object> map3=new HashMap<String, Object>();
+            map3.put("pic",R.drawable.elevensss);
+            map3.put("text","欧米茄零式时空狭缝 阿尔法篇三层");
+
+            HashMap<String,Object> map4=new HashMap<String, Object>();
+            map4.put("pic",R.drawable.twelvesss);
+            map4.put("text","欧米茄零式时空狭缝 阿尔法篇四层");
 
 
-        SimpleAdapter simpleAdapter=new SimpleAdapter(this,list_map,R.layout.list_items,new String[]{"pic","name"},new int[]{R.id.items_imageView1,R.id.items_textView1});
-        listView.setAdapter(simpleAdapter);
+            listItems.add(map1);
+            listItems.add(map2);
+            listItems.add(map3);
+            listItems.add(map4);
 
-        listView.setOnItemClickListener(this);
+            listItemAdapter = new SimpleAdapter(this, listItems, R.layout.list_items,
+                    new String[]{"pic", "text"}, new int[]{R.id.items_imageView1, R.id.items_textView1});
+
+
+
+
+
+
+
+
+
 
 
     }
-
-    @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            case 0:Intent Summ1=new Intent(this,DpsOfSummoner1Activity.class);
+                startActivity(Summ1);
+                break;
+            case 1:Intent Summ2=new Intent(this,DpsOfSummoner2Activity.class);
+                startActivity(Summ2);
+                break;
+            case 2: Intent Summ3=new Intent(this,DpsOfSummoner3Activity.class);
+                startActivity(Summ3);
+                break;
+            case 3: Intent Summ4=new Intent(this,DpsOfSummoner4Activity.class);
+                startActivity(Summ4);
+                break;
 
-        if(position==0){
-
-            Intent Summ1=new Intent(this,DpsOfSummoner1Activity.class);
-            startActivity(Summ1);
-        }
-        else if(position==1){
-            Intent Summ2=new Intent(this,DpsOfSummoner2Activity.class);
-            startActivity(Summ2);
-        }
-        else if(position==2){
-            Intent Summ3=new Intent(this,DpsOfSummoner3Activity.class);
-            startActivity(Summ3);
-        }
-        else if(position==3){
-            Intent Summ4=new Intent(this,DpsOfSummoner4Activity.class);
-            startActivity(Summ4);
         }
     }
+
+
+
 }
+
